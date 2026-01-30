67eb91408ddb2e7084b60b61f9116fbac388612c
Tom Glover <mrethol@gmail.com>
2026-01-30 13:49:33 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//TODO: add on load/ready functionality
+document.addEventListener("DOMContentLoaded", () => {
+console.log("ready!")
+
+})
+//TODO: add functionality to open the correct spell books
+function openSpellBook(){
+    console.log("How do you find the right spellbook?!")
+}
+//TODO: add functionality to change password
+//TODO: add functionality to change name
+//TODO: add functionality to summon familiar
+// have a function where you can load content into a "user array" -> easier to work with
